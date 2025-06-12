757103311d30c6d42716da73629d55b86086ebba
Reem Hoisman <reemho@edu.hac.ac.il>
2025-06-12 17:22:50 +0300

    started rest controller

--- src/main/java/com/example/ex4/service/PlanPackageService.java
+++ src/main/java/com/example/ex4/service/PlanPackageService.java
@@ -1,8 +1,10 @@
 package com.example.ex4.service;
 
+import com.example.ex4.dto.PlanPackageDTO;
 import com.example.ex4.entity.PlanPackage;
 import com.example.ex4.entity.ProviderProfile;
 import com.example.ex4.repository.PlanPackageRepository;
+import com.example.ex4.repository.ProviderProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import java.util.List;
 public class PlanPackageService {
     @Autowired
     private PlanPackageRepository planPackageRepository;
+    @Autowired
+    private ProviderProfileRepository providerProfileRepository;
 
     public List<PlanPackage> getAllPackagesByCategory(String category, String sortByPrice) {
         return planPackageRepository.findPlanPackagesByProviderProfile_Category(category).orElse(null);
@@ -20,7 +24,16 @@ public class PlanPackageService {
     public List<PlanPackage> getAllProviderPackages(ProviderProfile profile) {
         return planPackageRepository.findAllByProviderProfile(profile).orElse(null);
     }
-    public void saveNewPackage(PlanPackage planPackage){
-        planPackageRepository.save(planPackage);
+    public void saveNewPackage(String username,PlanPackageDTO planPackage){
+        ProviderProfile providerProfile = providerProfileRepository.findProviderProfileByAppUser_UserName(username).orElse(null);
+        System.out.println(planPackage);
+        PlanPackage newPackage = new PlanPackage();
+        newPackage.setProviderProfile(providerProfile);
+        newPackage.setPackageType(planPackage.packageType);
+        newPackage.setPrice(planPackage.price);
+        newPackage.setDescription(planPackage.description);
+        newPackage.setTitle(planPackage.title);
+        System.out.println(newPackage);
+        planPackageRepository.save(newPackage);
     }
 }
--- src/main/resources/static/js/planPackages.js
+++ src/main/resources/static/js/planPackages.js
@@ -1,30 +1,38 @@
-import axios from 'axios'
 (()=>{
     const WEEKLY_MONTH="once a week per month"
     const ONE_TIME="one time"
     const CUSTOM="custom";
     const openOfferBtn=document.getElementById("open-offer-btn")
-    const form=document.querySelector("form")
+    const form=document.getElementById("pkg-form")
+    const titleInput= document.getElementById('title')
+    const descriptionInput= document.getElementById('description')
+    const priceInput= document.getElementById('price')
+    const packageTypeInput= document.getElementById('packageType')
 
     //  const createOfferBtn=document.getElementById("create-offer-btn")
 
    const processInput = (()=>{
-       const titleInput= document.getElementById('title')
-       const descriptionInput= document.getElementById('description')
-       const priceInput= document.getElementById('price')
-       const packageTypeInput= document.getElementById('packageType')
+
 
        const handleSubmit= async(e)=>{
+           console.log("SUBMIT EVENT TRIGGERED!"); // זה חייב להדפס
+
            e.preventDefault()
-           const inputs={
-               title:titleInput.value.trim(),
-               description:descriptionInput.value.trim(),
-               price:priceInput.value.trim(),
-               packageType:packageTypeInput.value.trim()
+           const inputs = {
+               title: titleInput.value.trim(),
+               description: descriptionInput.value.trim(),
+               packageType: packageTypeInput.value.trim(),
+               price: priceInput.value.trim(), // המרה למספר
+           };
+
+           try {
+               console.log(inputs)
+               const response = await axios.post("/package-plan/add-package", inputs, {headers: { 'Content-Type': 'application/json'}});
+               console.log(response)
+               window.location = "/admin";
+           } catch (err) {
+               console.log("error", err);
            }
-           axios.post(form.action,inputs)
-               .then(res=>{window.location="/admin"})
-               .catch(()=>console.log("error"))
 
        }
        return{
@@ -35,6 +43,6 @@ import axios from 'axios'
 
 
     document.addEventListener("DOMContentLoaded",()=>{
-        form.addEventListener('submit',(e)=>processInput.handleSubmit(e))
+        form.addEventListener('submit',processInput.handleSubmit)
     })
 })()
